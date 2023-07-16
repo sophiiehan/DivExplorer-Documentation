bfc5af106eb69bd5b75fbd83eb99e47b0932d937
Sophie Han <32656696+sophiiehan@users.noreply.github.com>
2023-07-17 00:35:48 +0100

    Initial Commit

---
+++ source/conf.py
@@ -0,0 +1,27 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'DivExplorer-UserGuide'
+copyright = '2023, Eliana Pastor, Luca de Alfaro, Sophie Han, Harsh Dadhich'
+author = 'Eliana Pastor, Luca de Alfaro, Sophie Han, Harsh Dadhich'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'pydata_sphinx_theme'
+html_static_path = ['_static']
---
+++ source/index.rst
@@ -0,0 +1,20 @@
+.. DivExplorer-UserGuide documentation master file, created by
+   sphinx-quickstart on Sun Jul  2 22:21:47 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to DivExplorer-UserGuide's documentation!
+=================================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
